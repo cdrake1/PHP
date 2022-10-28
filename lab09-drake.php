de7461737e147ace9c481096017016f5c6b0d3b1
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2022-10-28 15:56:47 -0400

    lab9 start

---
+++ lab09-drake.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+
+        <title> Lab 9 Collin Drake </title>
+    </head>
+    <body>
+        <?php
+            echo "<h1> Lab 9 - PHP Functions </h1>";
+            echo "<br> <h5> Collin Drake </h5>";
+
+            require ("connect_db.php");
+            echo"<br>";
+
+
+
+        ?>
+
+        
+    </body>
+</html>

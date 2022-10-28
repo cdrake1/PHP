bcd74a36f9bb7ef8786193925781463973900c48
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2022-10-27 23:25:01 -0400

    C

---
+++ 09c-drake.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+
+        <title> Part C </title>
+    </head>
+    <body>
+        <?php
+        echo "<b><br> 9C - C. Drake </b><br>";
+        echo "<p> We first call connect_db.php to connect to site_db.";
+
+        require ("connect_db.php");
+        echo"<br>";
+
+        ?>
+        
+    </body>
+</html>

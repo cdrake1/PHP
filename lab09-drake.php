db5c546af879aed2b02ad2a1083c7aa4b939a25a
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2022-10-31 21:45:13 -0400

    lab 9 idk

--- lab09-drake.php
+++ lab09-drake.php
@@ -1,9 +1,6 @@
-<!-- Lab 9 PHP
-    Collin Drake October 29th 2022
--->
 <?php
-
-    $fname = "Collin";
+    # PHP Lab 9 Collin Drake October 29th 2022
+    
     require ("connect_db.php");
     echo"<br>";
 
@@ -13,22 +10,31 @@
     echo "<h1> Lab 9 - PHP Functions </h1>";
     echo FILE_AUTHOR;
 
-
-    $q = "show tables from site_db;";
+    $q = "USE site_db;";
     $r = mysqli_query ($dbc, $q);
-    if(!$r){
-        echo "DB error";
-        echo 'Mysql error: ' . mysql-error();        
-        exit;
+
+    if($r){
+        $q = "show tables;";
+        $r = mysqli_query ($dbc, $q);
+        echo "<li> Query Worked.</li>";
+        while($row = mysqli_fetch_row($r, MYSQLI_NUM)){
+            explain_table($row[0],$dbc);
+        }
     }
-    while($row = mysql-fethc_row($r)){
-        echo "table: {$row[0]}\n";
+    else{
+        echo "<li>" . mysqli_error( $dbc ) . "</li>" ;
     }
 
-    function explain_table($table_name, $dbc){
-        echo "<br> Table (in function):" .  $table_name;
-        echo '<br> The variable $fname is set to ' . $fname;
-
+    function explain_table($table, $dbc)
+    {
+        echo " table Name: $table ";
+        $q = "explain $table";
+        $r = mysqli_query ($dbc, $q);
+        if($r){
+            while($row = mysqli_fetch_row($r, MYSQLI_NUM)){
+                echo ($row[0] . " " . $row[1] . " " . $row[2] . " " . $row[3] . " " . $row[4] . " " . $row[5]. "<br>");
+            }
+        }
     }
 
 

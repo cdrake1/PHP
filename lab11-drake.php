41adddf13ddc1d043cd9aca954ec39a613a2a1a8
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2022-11-15 23:10:54 -0500

    not correct

--- lab11-drake.php
+++ lab11-drake.php
@@ -12,8 +12,10 @@
 
     <body>
         <?php
+
+            session_start();
             require ("connect_db.php");
-            require ("error_handler.php");
+            include ("error_handler.php");
 
             # We want to make sure this lab displays that it is registered under my name. A constant variable is the best way to do this.
             define('FILE_AUTHOR', 'COLLIN DRAKE');
@@ -22,7 +24,19 @@
             echo FILE_AUTHOR;
             echo "<hr>";
 
+            #show we are not logged in
+            if(ISSET($_SESSION['login_status']))
+            {
+                $login_status = $_SESSION['login_status'];
+            }
+            else
+            {
+                $login_status = "Not Logged In";
+            }
+
+
             #we want to check to see if the username and password are already set
+            $error_message = "";
             if (ISSET($_POST['username']))
             {
                 $username = $_POST['username'];
@@ -41,16 +55,7 @@
                 $password = "";
             }
 
-            #this form is used as a login for a username and password so we can implement this later into our project
-            echo "<form action= '". $_SERVER['SCRIPT_NAME'] ."' method='POST'>";
-            echo "<br> Enter your Username <input type='text' value='$username' >";
-            echo "<br> Enter your Password <input type='password' value='$password' >";
-            echo "<br><input type='submit' value='Submit!' style='color: blue;'>";
-            echo "</form>";
-            echo "<br>";
-
-            #error message checking
-            $error_message = "";
+            #error message checking if left blank
             if($_SERVER["REQUEST_METHOD"] == "POST")
             {
                 if($username = "")
@@ -61,24 +66,40 @@
                 {
                     $error_message = " No Password provided";
                 }
-
+                if($error_message == "")
+                {
+                    echo "<br> Welcome $username";
+                    $_SESSION["Login_status"] = "Logged In";
+                    $login_status = $_SESSION["Login_status"];
+                }
             }
             echo $error_message;
+            echo "<br>" . $login_status;
+
+            if($_SERVER['REQUEST_METHOD'] == 'GET' || $error_message != " " || $_SESSION['Login_status']!= "Logged In")
+            {
+                #this form is used as a login for a username and password so we can implement this later into our project
+                echo "<form action= '". $_SERVER['SCRIPT_NAME'] ."' method='POST'>";
+                echo "<br> Enter your Username <input type='text' name= 'username' value='$username' >";
+                echo "<br> Enter your Password <input type='password' name= 'password' value='$password' >";
+                echo "<br><input type='submit' value='Submit!' style='color: blue;'>";
+                echo "</form>";
+                echo "<br>";
+            }
 
             validation($dbc. $username, $password);
-            
+
+            #for some reason lets user in for any username and password
             #function to determine if the form entry was a valid entry.  We check this way using our sql database and if the entry already exists
             function validation($dbc, $username1, $password1)
             {
-                $q = "SELECT * FROM PrintUsers";
+                $q = "SELECT * FROM PrintUsers WHERE user_name = '$username1' AND user_password = '$password1' ";
                 $r = mysqli_query ( $dbc , $q );
                 if($r)
                 {
-                    $row_count = $r->num_rows;
-
-                    if($row_count != 0)
+                    if(mysqli_num_rows($r) == 0)
                     {
-                        $error_message = "This entry already exists";
+                        $error_message = "Invalid username/password combination";
                     }
                 }
                 else
@@ -87,7 +108,14 @@
                 }
 
             }
+        
+            #logged in using POST method
+            if($_SERVER['REQUEST_METHOD'] == 'POST' & $error_message != " ")
+            {
+                echo "<br> User $user_name successfully logged in";
+                $_SESSION["Login_status"] = "LOGGED IN";
 
+            }
             # a footer file to distinguish this as my work
             require ("footer.php");
         ?>
